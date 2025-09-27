765477fecef2ffe57a9e3d399ee58cecd62c8bcd
charlie <sxergiu@gmail.com>
2025-09-27 19:01:55 +0300

    empty spring boot app

---
+++ promptle/src/main/java/com/app/promptle/PromptleApplication.java
@@ -0,0 +1,13 @@
+package com.app.promptle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptleApplication.class, args);
+	}
+
+}
---
+++ promptle/src/test/java/com/app/promptle/PromptleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.promptle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
