6211d4096ba5d5fdb6b79068a42f617be1924f4f
charlie <sxergiu@gmail.com>
2025-11-15 18:00:30 +0200

    sample routing and landing page

--- promptle-app/src/app/app.routes.ts
+++ promptle-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {RoomGeneratorComponent} from './room-generator/room-generator.component';
+import {ImageGeneratorComponent} from './image-generator/image-generator.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'room-generator', component: RoomGeneratorComponent },
+  { path: 'image-generator', component: ImageGeneratorComponent },
+  { path: '**', redirectTo: '' }
+];
