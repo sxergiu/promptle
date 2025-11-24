c085230670a225cbbc51b726371f073aee1ccf87
sergiu <sergiu.varga@irian.ro>
2025-11-24 16:11:00 +0200

    basic sample player page

--- promptle-app/src/app/player-page/player-page.component.ts
+++ promptle-app/src/app/player-page/player-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-player-page',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './player-page.component.scss'
 })
 export class PlayerPageComponent {
+  readonly username = signal('');
+  readonly selectedIcon = signal('player-icons/saxon-player.svg');
+
+  handleUsernameInput(value: string): void {
+    this.username.set(value);
+  }
+
+  randomizeIcon(): void {
+    // Placeholder until more icons exist
+  }
+
+  usernameIsValid(): boolean {
+    return this.username().trim().length > 0;
+  }
 
 }
