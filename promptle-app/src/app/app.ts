29cea338d1ad829da260f233a1b14f355af77899
charlie <sxergiu@gmail.com>
2026-03-08 17:14:30 +0200

    added dark/light mode switch + theme

--- promptle-app/src/app/app.ts
+++ promptle-app/src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './service/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.scss']
 })
 export class App {
-
+  protected theme = inject(ThemeService);
 }
