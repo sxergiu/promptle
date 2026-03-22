c3331d6ac5caa8d782d275115a0eb48f4cff9e8f
sergiu <sergiu.varga@irian.ro>
2026-03-22 11:02:34 +0200

    Game JPARepositories

---
+++ promptle/src/main/java/com/app/promptle/game/repository/RoundAssignmentRepository.java
@@ -0,0 +1,17 @@
+package com.app.promptle.game.repository;
+
+import com.app.promptle.game.model.RoundAssignment;
+import com.app.promptle.room.model.Player;
+import com.app.promptle.room.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RoundAssignmentRepository extends JpaRepository<RoundAssignment, UUID> {
+
+    Optional<RoundAssignment> findByRoomAndRoundAndPlayer(Room room, int round, Player player);
+
+    List<RoundAssignment> findByRoom(Room room);
+}
