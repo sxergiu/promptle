142bd54633da449a2e8b73b77fe81c1769b8a0ed
sergiu <sergiu.varga@irian.ro>
2026-04-17 18:25:56 +0300

    more icons

--- promptle-app/src/app/core/models/player-icons.ts
+++ promptle-app/src/app/core/models/player-icons.ts
@@ -6,5 +6,12 @@ export interface PlayerIcon {
 export const PLAYER_ICONS: PlayerIcon[] = [
   { id: 'icon-1', path: 'player-icons/red-saxon.svg' },
   { id: 'icon-2', path: 'player-icons/saxon.svg' },
-  { id: 'icon-3', path: 'player-icons/brown-saxon.svg' }
+  { id: 'icon-3', path: 'player-icons/brown-saxon.svg' },
+  { id: 'icon-4', path: 'player-icons/dark-wiz.svg' },
+  { id: 'icon-5', path: 'player-icons/red-wiz.svg' },
+  { id: 'icon-6', path: 'player-icons/blue-wiz.svg' },
+  { id: 'icon-7', path: 'player-icons/green-wiz.svg' },
+  { id: 'icon-8', path: 'player-icons/boy-cone.svg' },
+  { id: 'icon-9', path: 'player-icons/cat-cone.svg' },
+  { id: 'icon-10', path: 'player-icons/wiz-cone.svg' },
 ];
